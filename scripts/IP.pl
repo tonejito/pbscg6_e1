d71da4a337c9de1a30563b203289782896a68bc4
Andres Hernandez <tonejito.doesexist@gmail.com>
2011-08-29 01:28:31 -0500

    Housekeeping

--- scripts/IP.pl
+++ scripts/IP.pl
@@ -3,150 +3,19 @@
 #Proyecto Programacion en Perl
 
 use strict;
-use Net::Abuse::Utils qw( :all );
-
-package archivos;
-	our $file;
-	our $blackfile;
-	our $FILE;
-	our $BLACKFILE;
 
 package patrones;
-	our %ip;
-	our $tot_ip = 0;
-	our %lista;
-	our $tot_lista = 0;
-        
-
-#Función para determinar el número de argumentos
-sub cuentaArgumentos{
-	if(@ARGV < 2 ){
-		print "USO: $0 --file=FILE --blacklist=FILE\n";
-		exit;
-	}
-}
-
-#Función para obtener y verificar el archivo a buscar y el archivo de lista negra
-sub obtenArchivos{
-	if($ARGV[0] =~ m/((--file=).*)/){
-		$archivos::file=$' if $ARGV[0] =~ m/=/;
-	}else{
-		print "USO: $0 --file=FILE --blacklist=FILE\n";
-		exit;
-	}
-	if($ARGV[1] =~ m/((--blacklist=).*)/){
-		$archivos::blackfile=$' if $ARGV[1] =~ m/=/;
-	}else{
-		print "USO: $0 --file=FILE --blacklist=FILE \n";
-		exit;
-	}
-
-	if(-e $archivos::file){
-		if(-f $archivos::file){
-			print "File: $archivos::file\n";
-		}
-		else{
-			print "El archivo '$archivos::file' no es un archivo regular\n";
-			exit;
-		}
-	}
-	else{
-		print "No se pudo encontrar el archivo '$archivos::file'\n";
-		exit;
-	}
-
-	if(-e $archivos::blackfile){
-		if(-f $archivos::blackfile){
-			print "Blacklist File: $archivos::blackfile\n";
-		}
-		else{
-			print "El archivo '$archivos::blackfile' no es un archivo regular\n";
-			exit;
-		}
-	}
-	else{
-		print "No se pudo encontrar el archivo '$archivos::blackfile'\n";
-		exit;
-	}
-
-}
-
-
-sub imprimeResultados{
-my $tope = 20;
-my $k = 0;
-	if($patrones::tot_ip != 0){
-	print " ____________________________________________________________________________\n";
-	print "\|	TOP	20     DIRECCIONES   IP     FUENTE    DE     SPAM           \|\n";
-	print "\|___________________________________________________________________________\|\n";
-	print "\|	Direccion IP			|		Incidencias         \|\n";
-	print "\|_______________________________________|___________________________________\|\n";
-
-		foreach  (reverse sort {$patrones::ip{$a} <=> $patrones::ip{$b}} keys %patrones::ip){
-			if($k <= $tope){
-				print "|		$	_		|     			$patrones::ip{$_}  	    |";
-				print "\n";
-			}
-			$k++;
-		}
-	print "\|_______________________________________|___________________________________\|\n";
-	}
-	print "\n";
-	$k = 0;
-	if($patrones::tot_ip !=0){
-	print " _______________________________________________________________________________________________________________\n";
-	print "\|		                  		IP's Identificadas en Listas Negras			       \|\n";
-	print "\|_____________________________________________________________________________________________________________ \|\n";
-	print "\|	Direccion IP			|		Incidencias         |		Black List	       \|\n";
-	print "\|_______________________________________|___________________________________|__________________________________\|\n";
-				
-		foreach  (reverse sort {$patrones::ip{$a} <=> $patrones::ip{$b}} keys %patrones::ip){
-                        if($k <= $tope){
-                        print "|                $       _               |                       $patrones::ip{$_}           |";
-                                print "\n";
-                        }
-                        $k++;
-                }
-	print "\|_______________________________________|___________________________________|__________________________________\|\n";
-
-	}
-
-}
-
-#Función para evaluar las opciones ingresadas
-sub ejecutaOpciones{
-	if(@ARGV == 2){
-		open($archivos::FILE,"$archivos::file") || die "ERROR al abrir el archivo $archivos::file";
-		$componentes::lineas=0;
-		while(<$archivos::FILE>){
-			chomp($_);
-			ip($_);
-			$componentes::lineas++;
-		}
-		imprimeResultados();
-	#	listaNegra();
-        	close($archivos::FILE);
-	}
-	else{
-        	for(my $i=2;$i<@ARGV;$i++){
-	                if($ARGV[$i] eq "--ip"){
-				open($archivos::FILE,"$archivos::file") || die "ERROR al abrir el archivo $archivos::file";
-				$componentes::lineas=0;
-				while(<$archivos::FILE>){
-					chomp($_);
-					ip($_);
-					$componentes::lineas++;
-				}
-        			close($archivos::FILE);
-	                }#Fin ip
-			else{
-				print "Opción $ARGV[$i] no válida\n";
-			}
-        	}#Fin for
-		close($archivos::FILE);
-	}#Fin else
-}
-
+our $lineas = 0;
+our $file;
+our $blackfile;
+our $FILE;
+our $BLACKFILE;
+our $ipregexp = '/((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))/';
+our %ip;
+our $tot_ip = 0;
+our $tot_lista = 0;
+our %blacklist;
+our $blacklist_message = "\t OK\n";
 
 ####### INICIO DEL PROGRAMA PRINCIPAL #######
 cuentaArgumentos(); #Verificación de argumentos
@@ -155,28 +24,196 @@ obtenArchivos(); #Se obtienen los archivos
 
 ejecutaOpciones(); #Se ejecutan las opciones que se le pasen al programa
 
+#Función para determinar el número de argumentos
+sub cuentaArgumentos
+{
+	if(@ARGV < 2 )
+	{
+		print "USO: $0 --file=FILE --blacklist=FILE\n";
+		exit;
+	}
+}
+
+#Función para obtener y verificar el archivo a buscar y el archivo de lista negra
+sub obtenArchivos
+{
+	if($ARGV[0] =~ m/((--file=).*)/)
+	{
+		$file=$' if $ARGV[0] =~ m/=/;
+	}
+	else
+	{
+		print "USO: $0 --file=FILE --blacklist=FILE\n";
+		exit;
+	}
+	if($ARGV[1] =~ m/((--blacklist=).*)/)
+	{
+		$blackfile=$' if $ARGV[1] =~ m/=/;
+	}
+	else
+	{
+		print "USO: $0 --file=FILE --blacklist=FILE \n";
+		exit;
+	}
+
+	if(-e $file)
+	{
+		if(-f $file)
+		{
+			print "File: $file\n";
+		}
+		else
+		{
+			print "El archivo '$file' no es un archivo regular\n";
+			exit;
+		}
+	}
+	else
+	{
+		print "No se pudo encontrar el archivo '$file'\n";
+		exit;
+	}
+
+	if(-e $blackfile)
+	{
+		if(-f $blackfile)
+		{
+			print "Blacklist File: $blackfile\n";
+		}
+		else
+		{
+			print "El archivo '$blackfile' no es un archivo regular\n";
+			exit;
+		}
+	}
+	else
+	{
+		print "No se pudo encontrar el archivo '$blackfile'\n";
+		exit;
+	}
+
+}
+
+#Función para evaluar las opciones ingresadas
+sub ejecutaOpciones
+{
+	if(@ARGV == 2)
+	{
+		open($FILE,"$file") || die "ERROR al abrir el archivo $file";
+		$lineas=0;
+		while(<$FILE>)
+		{
+			chomp($_);
+			ip($_);
+			$lineas++;
+		}
+		imprimeResultados($_);
+		construyeListaNegra();
+        	close($FILE);
+	}
+	else
+	{
+        	for(my $i=2;$i<@ARGV;$i++)
+        	{
+	                if($ARGV[$i] eq "--ip")
+	                {
+				open($FILE,"$file") || die "ERROR al abrir el archivo $file";
+				$lineas=0;
+				while(<$FILE>){
+					chomp($_);
+					ip($_);
+					$lineas++;
+				}
+        			close($FILE);
+	                }#Fin ip
+			else
+			{
+				print "Opción $ARGV[$i] no válida\n";
+			}
+        	}#Fin for
+		close($FILE);
+	}#Fin else
+}
+
+sub imprimeResultados
+{
+	my $tope = 20;
+	my $k = 0;
+	if($tot_ip != 0)
+	{
+	print " ____________________________________________________________________________\n";
+	print "\|	TOP	20     DIRECCIONES   IP     FUENTE    DE     SPAM           \|\n";
+	print "\|___________________________________________________________________________\|\n";
+	print "\|	Direccion IP			|		Incidencias         \|\n";
+	print "\|_______________________________________|___________________________________\|\n";
+
+		foreach  (reverse sort {$ip{$a} <=> $ip{$b}} keys %patrones::ip)
+		{
+			if($k <= $tope)
+			{
+				print "|		$	_		|     			$ip{$_}  	   |";
+				print "\n";
+			}
+			$k++;
+		}
+	print "\|_______________________________________|___________________________________\|\n";
+	}
+	print "\n";
+	$k = 0;
+	if($tot_ip !=0)
+	{
+	print " _______________________________________________________________________________________________________________\n";
+	print "\|		                  		IP's Identificadas en Listas Negras			       \|\n";
+	print "\|_____________________________________________________________________________________________________________ \|\n";
+	print "\|	Direccion IP			|		Incidencias         |	     Black List y C&C	       \|\n";
+	print "\|_______________________________________|___________________________________|__________________________________\|\n";
+				
+		foreach  (reverse sort {$ip{$a} <=> $ip{$b}} keys %patrones::ip)
+		{
+                        if($k <= $tope )
+                        {
+				 print "|                $       _              |       $ip{$_}           |		  		|";
+                                print "\n"; 
+                        }
+                        $k++;
+                }
+	print "\|_______________________________________|___________________________________|__________________________________\|\n";
+	}
+
+}
+
+#construye la lista negra
+sub construyeListaNegra
+{
+	open(BLACKFILE, "$blackfile") || die ("No se pudo abrir el archivo:",$blackfile);
+	while(<BLACKFILE>)
+	{
+		chomp();
+		if($_ =~ /((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))/)
+		{
+			$blacklist{$_}++;
+		}
+	}
+}
 
 #Función para detectar IP's
-sub ip{
+sub ip
+{
         my $linea0 = shift;
-        if($linea0 =~ /((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))/) {
-                $patrones::ip{$1}++;
-                $patrones::tot_ip++;
+        if($linea0 =~ /((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}))/)
+        {
+                $ip{$1}++;
+                $tot_ip++;
         }
 }
 
-#Función para comparar con la lista negra
-sub comparaBlackList{
-	open($archivos::BLACKFILE,"$archivos::blackfile") || die "ERROR al abrir el archivo $archivos::blackfile";
-       my  $elemento = shift;
-        while(<$archivos::BLACKFILE>){
-                chomp($_);
-                if($elemento =~ m/$_/){
-                        print "--> LISTA NEGRA";
-			last;
-			#$hash++
-                }
-        }
-	close($archivos::BLACKFILE);
+#checa si está en lista negra
+sub isBlacklisted
+{
+	my $item = shift;	
+	foreach my $element (keys %blacklist)
+	{
+		return $item if ($item eq $element);
+	}
 }
 
--- scripts/URL.pl
+++ scripts/URL.pl
@@ -1,152 +1,97 @@
 #!/usr/bin/perl -w
 
+#proyecto programacion en Perl
+
 use strict;
 
-package vars;
-	our $file;
-	our $blackfile;
-	our $FILE;
-	our $BLACKFILE;
-
 package patrones;
-	our %url;
-	our $tot_url = 0;
-	our $i;
-	our %data;
-# Top Level Domain	
+our $file;
+our $blackfile;
 our $tld = '(a(ero|rpa|sia)|biz|c(at|om|oop)|edu|gov|i(nfo|nt)|jobs|m(il|obi|useum)|n(a(me|to)|et)|org|pro|t(el|ravel))';
-# Country Code Top Level Domain	
 our $cctld = '((a(c|d|e|f|g|i|l|m|n|o|q|r|s|t|u|w|x|z))|(b(a|b|d|e|f|g|h|i|j|m|n|o|r|s|t|v|w|y|z))|(c(a|c|d|f|g|h|i|k|l|m|n|o|r|s|u|v|x|y|z))|(d(d|e|j|k|m|o|z))|(e(c|e|g|h|r|s|t|u))|(f(i|j|k|m|o|r))|(g(a|b|d|e|f|g|h|i|l|m|n|p|q|r|s|t|u|w|y))|(h(k|m|n|r|t|u))|(i(d|e|l|m|n|o|q|r|s|t))|(j(e|m|o|p))|(k(e|g|h|i|m|n|p|r|w|y|z))|(l(a|b|c|i|k|r|s|t|u|v|y))|(m(a|c|d|e|g|h|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z))|(n(a|c|e|f|g|i|l|o|p|r|u|z))|(o(m))|(p(a|e|f|g|h|k|l|m|n|r|s|t|w|y))|(q(a))|(r(e|o|s|u|w))|(s(a|b|c|d|e|g|h|i|j|k|l|m|n|o|r|t|u|v|y|z))|(t(c|d|f|g|h|j|k|l|m|n|o|p|r|t|v|w|z))|(u(a|g|k|s|y|z))|(v(a|c|e|g|i|n|u))|(w(f|s))|(y(e|t))|(z(a|m|w)))';
-# Dominio de internet
 our $domain_regex = '[\w\d]+(\.?[\w\d\-])*\.'.$tld.'(\.'.$cctld.')?';
 our $protocol_regex = '(f|ht)tps?://' ;
 our $path_regex = '[\w\d\-\/\+_.,:=#%]+';
-# URL de http, https o ftp
 our $url_regex = $protocol_regex.'('.$domain_regex.')'.$path_regex;
+our %url;
+our $tot_url;
+our %direcciones = ();
+our %blacklistedURL = ();
 
-#Función para determinar el número de argumentos
-sub cuentaArgumentos(){
-	if(@ARGV < 2 ){
-		print "USO: $0 --file=FILE --blacklist=FILE\n";
-		exit;
+#logica del programa
+
+open($file,"$ARGV[0]");
+
+while(<$file>){
+	chomp($_);
+	if ($_ =~ /($url_regex)/)
+	{
+		$patrones::url{$1}++;
+		$patrones::tot_url++;
 	}
 }
 
-#Función para obtener y verificar el archivo a buscar y el archivo de lista negra
-sub obtenArchivos(){
-	if($ARGV[0] =~ m/((--file=).*)/){
-		$archivos::file=$' if $ARGV[0] =~ m/=/;
-	}else{
-		print "USO: $0 --file=FILE --blacklist=FILE\n";
-		exit;
-	}
-	if($ARGV[1] =~ m/((--blacklist=).*)/){
-		$archivos::blackfile=$' if $ARGV[1] =~ m/=/;
-	}else{
-		print "USO: $0 --file=FILE --blacklist=FILE \n";
-		exit;
-	}
+muestraResultados();
 
-	if(-e $archivos::file){
-		if(-f $archivos::file){
-			print "File: $archivos::file\n";
-		}
-		else{
-			print "El archivo '$archivos::file' no es un archivo regular\n";
-			exit;
-		}
-	}
-	else{
-		print "No se pudo encontrar el archivo '$archivos::file'\n";
-		exit;
-	}
 
-	if(-e $archivos::blackfile){
-		if(-f $archivos::blackfile){
-			print "Blacklist File: $archivos::blackfile\n";
-		}
-		else{
-			print "El archivo '$archivos::blackfile' no es un archivo regular\n";
-			exit;
-		}		
-	}
-	else{
-		print "No se pudo encontrar el archivo '$archivos::blackfile'\n";
-		exit;
-	}
+close($file);
 
+#funciones del programa
+
+sub muestraResultados
+{
+	if($tot_url != 0) {
+				foreach my $elemento (sort{$patrones::url{$b} <=> $patrones::url{$a}} keys %patrones::url)
+				{ 
+					print  "|	$elemento	| $patrones::url{$elemento}	|	|\n";
+				}
+						
+	}
 }
 
-sub imprimeResultados(){
-	if($patrones::tot_url != 0){
-                print "\n************************\nURLS\n--------------\n";
-                foreach my $elemento (sort keys%patrones::url){
-                        print "$elemento -  $patrones::url{$elemento}        ";
-                        comparaBlackList($elemento);
-                        print "\n";
-                }
-                print "Se encontraron $tot_url URLs\n";
-        }
-
+sub isBlackListedURL
+{
+	my $direccion = shift;
+	foreach my $entrada (keys %blacklist_url)
+	{
+		return $direccion if ($direccion eq $entrada);
+	}
 }
 
-sub ejecutaOpciones(){
-	if(@ARGV == 2){
-		open($archivos::FILE,"$archivos::file") || die "ERROR al abrir el archivo $archivos::file";
-		$componentes::lineas=0;
-		while(<$archivos::FILE>){
-			chomp($_);
-			url($_);
-			$componentes::lineas++;
-		}
-		imprimeResultados();
-        	close($archivos::FILE);
-	}elsif($ARGV[$i] eq "--url"){
-				open($archivos::FILE,"$archivos::file") || die "ERROR al abrir el archivo $archivos::file";
-				$componentes::lineas=0;
-				while(<$archivos::FILE>){
-                                        chomp($_);
-                                        url($_);
-                                        $componentes::lineas++;
-                                }
-                                close($archivos::FILE);
-	                		}#Fin url
-			else{
-				print "Opción $ARGV[$i] no válida\n";
-			}
-		
-		close($archivos::FILE);
-				}#Fin else
-
-
-
-####### INICIO DEL PROGRAMA PRINCIPAL #######
-cuentaArgumentos(); #Verificación de argumentos
-
-obtenArchivos(); #Se obtienen los archivos
-
-ejecutaOpciones(); #Se ejecutan las opciones que se le pasen al programa
-
-####### FIN DEL PROGRAMA PRINCIPAL #######
-
-sub url(){ #Función para detectar URL's
-	my $linea2 = shift;
-        if($linea2 =~ /$url_regex/){
-                $patrones::url{$2}++;
-                $patrones::tot_url++;
-        }
+sub blacklistedSourceURLs
+{
+	foreach my $direccion (keys %direcciones)
+	{
+		$blacklistedURL{$direccion}++ if($direccion and isBlacListedURL($direccion));
+	}
 }
 
-#Función para comparar con la lista negra
-sub comparaBlackList(){
-	open($archivos::BLACKFILE,"$archivos::blackfile") || die "ERROR al abrir el archivo $archivos::blackfile";
-        my $elemento = shift;
-        while(<$archivos::BLACKFILE>){
-                chomp($_);
-                if($elemento =~ m/$_/){
-                        print "--> LISTA NEGRA";
-                }
-        }
-	close($archivos::BLACKFILE);
+sub printSourceURL
+{
+	#logmsg($LOGFILE,"source URLs");
+	#separator "source URL's";
+	foreach my $key (sort{$direcciones{$b} <=> $direcciones{$a}} keys %direcciones)
+	{
+		#logmsg($LOGFILE,$key."\t".$direcciones{$key}) if ($verbose);
+		print $key."\t".$direcciones{$key}."\n";
+	}
 }
+
+sub printBlackListedURL
+{
+	#logmsg($LOGFILE,"blacklisted URL's");
+	#separator "blacklisted URL's";
+	foreach my $key (sort{$blacklistedURL{$a} <=> $blacklistedURL{$b}} keys %blacklistedURL)
+	{
+		#logmsg($LOGFILE,$key."\t".$blacklistedURL{$key}) if ($verbose);	
+		print $key. "\t".$blacklistedURL{$key}."\n";
+	}
+}
+
+
+
+
+
+
+
 
